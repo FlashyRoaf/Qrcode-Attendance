944b153ce61da3779ccf8ed2030f1e6baca30af0
unknown <arielchristianwijaya@gmail.com>
2025-07-19 12:40:29 +0800

    Making qr code generator

---
+++ resources/js/pages/QRCodeAttendance.vue
@@ -0,0 +1,12 @@
+<template>
+    <img :src="qrcode" alt="Qr Code">
+</template>
+
+<script setup lang="ts">
+
+interface Props {
+    qrcode: string;
+}
+
+const props = defineProps<Props>();
+</script>
